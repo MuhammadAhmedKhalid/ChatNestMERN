8fe1e6f95d83dd58ff2585c1894e0684ee3c7eff
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-10-05 20:25:24 +0500

    Navbar responsiveness added.

--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -4,11 +4,11 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-left">
-        <a className="nav-link">ChatNest</a>
+        <p className="nav-link">ChatNest</p>
       </div>
       <div className="navbar-right">
-        <a className="nav-link">Sign In</a>
-        <a className="nav-link">Sign Up</a>
+        <p className="nav-link">Sign In</p>
+        <p className="nav-link">Sign Up</p>
       </div>
     </nav>
   )
