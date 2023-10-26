79cd93182b9b73e611db0500e653050ad8171032
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-10-27 01:05:32 +0500

    Active users portion added for desktop view.

--- frontend/src/Components/HomeScreen/Container.tsx
+++ frontend/src/Components/HomeScreen/Container.tsx
@@ -3,7 +3,12 @@ import '../../Styling/HomeContainer.css'
 function Container() {
   return (
     <div className='center-box'>
-
+      <div className='center-content'>
+        
+      </div>
+      <div className='active-users'>
+        <h4>ACTIVE USERS</h4>
+      </div>
     </div>
   )
 }
